738802d3c2b75abd6f63b2607f2068e9b117aa1a
JudithKnight123 <judithknight.roles@gmail.com>
2026-06-18 17:14:30 +0100

    bug fixing notepad function appears to work now

--- week-4/assets/js/script.js
+++ week-4/assets/js/script.js
@@ -1,32 +1,6 @@
-// // 1. use document.getElementById to select the searchTerm Button
-// var searchTerm = document.getElementById("searchTerm");
-// // 2. use document.getElementById to select the searchButton Button
-// var searchButton = document.getElementById("searchButton");
-
-// // 3. add an event listener to the searchButton that calls the search function when clicked
-// searchButton.addEventListener("click", onClickSearchButton);
-
-// function onClickSearchButton() {
-//   // 4. use the value property of the searchInput to get the search term
-//   //TODO:
-//   var searchTerm = searchInput.value;
-  
-//   // 5. select the searches div using document.getElementById
-//   var searchesDiv = document.getElementById("searches");
-//   //TODO:
-//   // 6. create a new li element using document.createElement
-//   var newLi = document.createElement("li");
-//   //TODO:
-//   // 7. set the innerHTML of the new paragraph to the search term
-
-//   //TODO:
-//   // 8. append the new paragraph to the searches div
-//   //TODO:
-// }
-
 
 // 1. use document.getElementById to select the searchTerm Button
-var saveTerm = document.getElementById("saveTerm");
+var searchInput = document.getElementById("saveTerm");
 // 2. use document.getElementById to select the searchButton Button
 var saveButton = document.getElementById("saveButton");
 // 3. add an event listener to the searchButton that calls the search function when clicked
@@ -35,9 +9,9 @@ saveButton.addEventListener("click", onClickSaveButton);
 function onClickSaveButton() {
   // 4. use the value property of the searchInput to get the search term
   //TODO:
-  var searchInput = saveTerm.value;
+  var saveTerm = searchInput.value;
   
-  if (searchInput === "") {
+  if (saveTerm === "") {
   alert("Please enter a task before adding it.");
   return;
 }
@@ -48,18 +22,17 @@ function onClickSaveButton() {
   var newLi = document.createElement("li");
   //TODO:
   // 7. set the innerHTML of the new paragraph to the search term
-newLi.innerHTML = searchInput;
+newLi.innerHTML = searchInput.value;
   //TODO:
   // 8. append the new paragraph to the searches div
 searchesDiv.appendChild(newLi);
   //TODO:
   // 9. Button click event - create a delete button
-var clickButton = document.getElementById("clickButton");
+var clickButton = document.createElement("clickButton");
 clickButton.innerHTML = "Delete";
 clickButton.addEventListener("click", function () {
   newLi.remove();
 });
 newLi.appendChild(clickButton); //put Button inside li
 
-saveTerm.value = "";
 }
